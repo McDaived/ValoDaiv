b48e123a218535e6d76f3af3681251bd50c33fc6
Daived <mc.daived1996@gmail.com>
2023-07-14 11:16:52 +0300

    Update Sidebar.jsx

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -14,13 +14,13 @@ const Sidebar = ({ pageSetter }) => {
         app.quit();
     }
     return (
-        <div className='Sidebar bg-gray-800 h-[100%] w-[30%] relative flex flex-col p-3 gap-3 text-red'>
-            <button className='NoDrag w-[100%] bg-red-700 hover:bg-black-600 p-3 rounded-lg cursor-pointer transition-all duration-100 ease-linear relative flex justify-center items-center' onClick={() => { pageSetter(1) }}><GoLock className='absolute left-3' />Insta Locker</button>
-            <button className='NoDrag w-[100%] bg-red-700 hover:bg-black-600 p-3 rounded-lg cursor-pointer transition-all duration-100 ease-linear relative flex justify-center items-center' onClick={() => { pageSetter(3) }}><GiCurvyKnife className='absolute left-3' />Skin Changer</button>
-            <button className='NoDrag w-[100%] bg-red-700 hover:bg-black-600 p-3 rounded-lg cursor-pointer transition-all duration-100 ease-linear relative flex justify-center items-center' onClick={() => { pageSetter(2) }}><GoPencil className='absolute left-3' />Rank Editor</button>
-            <button className='NoDrag w-[100%] bg-red-700 hover:bg-black-600 p-3 rounded-lg cursor-pointer transition-all duration-100 ease-linear relative flex justify-center items-center' onClick={() => { pageSetter(4) }}><GoGear className='absolute left-3' />Authorize Account</button>
-            <button className='NoDrag w-[100%] bg-red-700 hover:bg-black-600 p-3 rounded-lg cursor-pointer transition-all duration-100 ease-linear relative flex justify-center items-center mt-auto' onClick={() => { shell.openExternal('https://discordapp.com/users/314721544901361664') }}><BsDiscord className='absolute left-3' />Discord</button>
-            <button className='NoDrag w-[100%] bg-red-700 hover:bg-black-600 p-3 rounded-lg cursor-pointer transition-all duration-100 ease-linear relative flex justify-center items-center' onClick={exitApp}><GoSignOut className='absolute left-3' />Exit</button>
+        <div className='Sidebar bg-red-800 h-[100%] w-[30%] relative flex flex-col p-3 gap-3 text-white'>
+            <button className='NoDrag w-[100%] bg-black hover:bg-red-500 p-3 rounded-lg cursor-pointer transition-all duration-100 ease-linear relative flex justify-center items-center' onClick={() => { pageSetter(1) }}><GoLock className='absolute left-3' />Insta Locker</button>
+            <button className='NoDrag w-[100%] bg-black hover:bg-red-500 p-3 rounded-lg cursor-pointer transition-all duration-100 ease-linear relative flex justify-center items-center' onClick={() => { pageSetter(3) }}><GiCurvyKnife className='absolute left-3' />Skin Changer</button>
+            <button className='NoDrag w-[100%] bg-black hover:bg-red-500 p-3 rounded-lg cursor-pointer transition-all duration-100 ease-linear relative flex justify-center items-center' onClick={() => { pageSetter(2) }}><GoPencil className='absolute left-3' />Rank Editor</button>
+            <button className='NoDrag w-[100%] bg-black hover:bg-red-500 p-3 rounded-lg cursor-pointer transition-all duration-100 ease-linear relative flex justify-center items-center' onClick={() => { pageSetter(4) }}><GoGear className='absolute left-3' />Authorize Account</button>
+            <button className='NoDrag w-[100%] bg-black hover:bg-red-500 p-3 rounded-lg cursor-pointer transition-all duration-100 ease-linear relative flex justify-center items-center mt-auto' onClick={() => { shell.openExternal('https://discordapp.com/users/314721544901361664') }}><BsDiscord className='absolute left-3' />Discord</button>
+            <button className='NoDrag w-[100%] bg-black hover:bg-red-500 p-3 rounded-lg cursor-pointer transition-all duration-100 ease-linear relative flex justify-center items-center' onClick={exitApp}><GoSignOut className='absolute left-3' />Exit</button>
         </div>
     )
 }
